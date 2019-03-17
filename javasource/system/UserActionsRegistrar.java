8403be36b86fb38e6d12061498e9513b31e3bc89
Alistair <alistair@wobdev.co.uk>
2019-03-17 21:56:25 +0000

    Original functionality restored, need to build in the examples and tests to see it all works

--- javasource/system/UserActionsRegistrar.java
+++ javasource/system/UserActionsRegistrar.java
@@ -12,7 +12,10 @@ public class UserActionsRegistrar
   public void registerActions(IActionRegistrator registrator)
   {
     registrator.bundleComponentLoaded();
+    registrator.registerUserAction(gcp.actions.J_AddListenerToSubscription.class);
     registrator.registerUserAction(gcp.actions.J_LoadCredentialFile.class);
+    registrator.registerUserAction(gcp.actions.J_PublishToTopic.class);
+    registrator.registerUserAction(gcp.actions.J_RemoveListenerFromSubscription.class);
     registrator.registerUserAction(gcp.actions.J_StartPublisherAgent.class);
     registrator.registerUserAction(gcp.actions.J_StartRegistryAgent.class);
     registrator.registerUserAction(gcp.actions.J_StartSubscriberAgent.class);
